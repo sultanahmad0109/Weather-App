4e9c622aafc2b228a4b641b1c0eb1c119a0f6641
Sultan Ahmad <sultanahmad0109@gmail.com>
2021-02-11 23:08:40 +0530

    Practice with Heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '../templates/views'));
